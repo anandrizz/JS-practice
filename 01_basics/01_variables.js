f110d772566283a029d33738fd78d2b94e7fb429
Anand J <164165693+disastrouspeanut@users.noreply.github.com>
2025-06-13 20:28:56 +0000

    practiced variables in js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail="anand@gmail.com";
+var accountPassword = "12345"
+accountcity = "Jaipur"
+let accountstate
+
+
+accountEmail ="hc@gmaihuohiuhl.com"
+accountPassword="89797998989"
+accountcity="bengaluru"
+// accountId="7878"
+console.table([accountEmail,accountId,accountPassword,accountcity,accountstate]);
+
+
+// prefer not top use var , because of issue in block scope and functional scope
