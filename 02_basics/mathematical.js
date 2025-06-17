d61e4e4b9e35e5e26a1a4ecad8f4c1bf0df54b53
Anand J <164165693+anandrizz@users.noreply.github.com>
2025-06-17 09:15:13 +0000

    successfully commited

---
+++ 02_basics/mathematical.js
@@ -0,0 +1,42 @@
+// const score = 400
+// console.log(score);
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString);
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+// const othernumber=23.89978
+// console.log(othernumber.toPrecision(6));
+
+
+
+
+// Maths
+// console.log(Math.abs(-4));
+
+// console.log(Math.round(2.6));
+
+// console.log(Math.ceil(4.2));
+
+// console.log(Math.floor(4.9));
+
+// console.log(Math.max(8,3,9,3));
+
+// 4
+// 3
+// 5
+// 4
+// 9
+
+
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10000+1));
+
+
+
+
