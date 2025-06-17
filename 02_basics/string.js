946ab69b9bd61a2244e77ced97626c3122da876a
Anand J <164165693+anandrizz@users.noreply.github.com>
2025-06-17 08:50:56 +0000

    successfully commited

---
+++ 02_basics/string.js
@@ -0,0 +1,27 @@
+const name = "hitesh"
+const repocount = 50
+
+// console.log(`hello my name is ${name} and my repocount is ${repocount}`);
+
+// you can inject variable by wrting ${} syntax
+
+const gameName = new String("hiteshc")
+
+// console.log(gameName[3]);
+
+// console.log(gameName.toUpperCase());  HITESHC
+
+// console.log(gameName.indexOf ("t"));    2
+
+// const anotherstring= gameName.slice(-8,4);
+
+// console.log(anotherstring);
+
+// const newstringOne= "                  hitesh                 ";
+// console.log(newstringOne);
+// console.log(newstringOne.trim());
+
+
+
+
+
