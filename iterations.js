95f22e1e3ba97959f7ed77383d521c3920fb810b
Anand J <anandj45678@gmail.com>
2025-06-29 00:24:27 +0530

    successfully commited

--- iterations.js
+++ iterations.js
@@ -1,7 +1,26 @@
-const coding = ["js", "ruby", "java", "python", "cpp"]
+// const coding = ["js", "ruby", "java", "python", "cpp"]
 
-coding.forEach((val) => {
-    console.log(val);
+// coding.forEach((val) => {
+//     console.log(val);
 
 
-});
+// });
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        languagefilename: "Js"
+    }, {
+        languageName: "java",
+        languagefilename: "Js"
+
+    },
+    {
+        languageName: "cpp",
+        languagefilename: "c"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
